a9019aba6c96d0e3ffaf1f39904e4172cb6a9ee4
bisaek <59936920+bisaek@users.noreply.github.com>
2022-10-08 04:11:21 +0200

    Update fedora_install.sh

--- fedora_install.sh
+++ fedora_install.sh
@@ -3,4 +3,5 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim';
 mkdir ~/.config/nvim;
 cp neovim-config/init.vim ~/.config/nvim/;
-nvim ~/.config/nvim/init.vim;
+echo "neovim is installed"
+echo "nvim [filename]"

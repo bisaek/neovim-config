e71c661d9ccf4f1d38776aafedb488a3a8ad25de
bisaek <59936920+bisaek@users.noreply.github.com>
2022-10-08 04:18:38 +0200

    Create arch_install.sh

---
+++ arch_install.sh
@@ -0,0 +1,8 @@
+
+sudo pacman -S neovim;
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim';
+mkdir ~/.config/nvim;
+cp neovim-config/init.vim ~/.config/nvim/;
+echo "neovim is installed"
+echo "nvim [filename]"

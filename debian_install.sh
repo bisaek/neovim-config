d4f4f9a94b689530801afebffb2de622078a1060
bisaek <59936920+bisaek@users.noreply.github.com>
2022-10-08 04:11:04 +0200

    Update debian_install.sh

--- debian_install.sh
+++ debian_install.sh
@@ -1,7 +1,8 @@
 
-sudo apt install neovim -y;
+sudo apt-get install neovim -y;
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim';
 mkdir ~/.config/nvim;
 cp neovim-config/init.vim ~/.config/nvim/;
-nvim ~/.config/nvim/init.vim;
+echo "neovim is installed"
+echo "nvim [filename]"
